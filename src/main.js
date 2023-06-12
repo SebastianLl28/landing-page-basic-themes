49a7d29cad2bd7403ab7ec8815f453f0fd5f8dec
SebastianLl28 <llamucasebas@gmail.com>
2023-06-12 10:43:55 -0500

    landing-page-basic-theme v1.0

--- src/main.js
+++ src/main.js
@@ -1,50 +1,13 @@
-const sun = "https://www.uplooder.net/img/image/55/7aa9993fc291bc170abea048589896cf/sun.svg";
-const moon = "https://www.uplooder.net/img/image/2/addf703a24a12d030968858e0879b11e/moon.svg"
+const messageError = document.querySelector('.form__error')
+const form = document.querySelector('form')
 
-// tema que va a cambiar 
-var theme = "dark";
-
-// selector del root
-const root = document.querySelector(":root");
-
-// 
-const container = document.getElementsByClassName("theme")[0];
-console.log(container);
-
-// const themeIcon = document.getElementById("theme-icon");
-// container.addEventListener("click", setTheme);
-// function setTheme() {
-// switch (theme) {
-//     case "dark":
-//     setLight();
-//     theme = "light";
-//     break;
-//     case "light":
-//     setDark();
-//     theme = "dark";
-//     break;
-// }
-// }
-// function setLight() {
-// root.style.setProperty(
-//     "--bs-dark",
-//     "linear-gradient(318.32deg, #c3d1e4 0%, #dde7f3 55%, #d4e0ed 100%)"
-// );
-// container.classList.remove("shadow-dark");
-// setTimeout(() => {
-//     container.classList.add("shadow-light");
-//     themeIcon.classList.remove("change");
-// }, 300);
-// themeIcon.classList.add("change");
-// themeIcon.src = sun;
-// }
-// function setDark() {
-// root.style.setProperty("--bs-dark", "#212529");
-// container.classList.remove("shadow-light");
-// setTimeout(() => {
-//     container.classList.add("shadow-dark");
-//     themeIcon.classList.remove("change");
-// }, 300);
-// themeIcon.classList.add("change");
-// themeIcon.src = moon;
-// }
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    const regex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g
+    const email = document.querySelector('.form__input').value
+    if (!regex.test(email)) {
+        messageError.classList.add('form__error--active')
+        return;
+    }
+    messageError.classList.remove('form__error--active')
+})
